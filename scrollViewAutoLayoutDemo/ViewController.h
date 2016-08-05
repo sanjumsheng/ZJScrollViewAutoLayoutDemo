f9fe054847b8a722f2cfcf76e98ba879d9c6ba6e
zhangjun <zhangjun@yinzhenghao.local>
2016-08-05 16:09:34 +0800

    uiscrollivew autolayout init

---
+++ scrollViewAutoLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scrollViewAutoLayoutDemo
+//
+//  Created by zhangjun on 16/8/5.
+//  Copyright © 2016年 sanjunsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
